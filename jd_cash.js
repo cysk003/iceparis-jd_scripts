26220fe7ccfd7fdd33cc3864b1665f83c2b2303f
iceparis <80054093+iceparis@users.noreply.github.com>
2021-06-29 10:39:29 +0800

    Update jd_cash.js

--- jd_cash.js
+++ jd_cash.js
@@ -31,7 +31,7 @@ let helpAuthor = true;
 const randomCount = $.isNode() ? 5 : 5;
 let cash_exchange = false;//是否消耗2元红包兑换200京豆，默认否
 const inviteCodes = [
-  `YBoyZOSwYP8h82vUz3IQ0Ns@eU9Yae66bq0n8D_QwnJB1w@eU9YaOnjZq4kpzzcmHoR0A@eU9YEabGEqNwqyaqvwxl@eU9YEabGEqNwqyaqvwxl`
+  `YBoyZOSwYP8h82vUz3IQ0Ns@IhM_bu-1Yfkj8Wu6iw@eU9Yae66bq0n8D_QwnJB1w@eU9YaOnjZq4kpzzcmHoR0A`
 ]
 $.authorCode = []
 if ($.isNode()) {
