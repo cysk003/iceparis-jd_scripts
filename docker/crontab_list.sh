b78f0b884902d012e38d143f34cd4a9bb681675f
iceparis <80054093+iceparis@users.noreply.github.com>
2021-07-18 09:49:16 +0800

    移除失效

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -15,7 +15,7 @@
 # 天天加速
 8 0-23/3 * * * node /scripts/jd_speed.js >> /scripts/logs/jd_speed.log 2>&1
 # 点点券(活动时间：2021-06-21至2021-11-21)
-10 0,20 * * * node /scripts/jd_necklace.js >> /scripts/logs/jd_necklace.log 2>&1
+# 10 0,20 * * * node /scripts/jd_necklace.js >> /scripts/logs/jd_necklace.log 2>&1
 # 省钱大赢家之翻翻乐(活动时间：2021-07-15至2021-8-15)
 20 * * * * node /scripts/jd_big_winner.js >> /scripts/logs/jd_big_winner.log 2>&1
 ##############长期活动##############
