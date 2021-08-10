68a126147326641d079eda3b219ef5248cf88d1c
iceparis <80054093+iceparis@users.noreply.github.com>
2021-08-10 08:34:03 +0800

    Update crontab_list.sh

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -7,7 +7,7 @@
 # 手机狂欢城 活动时间：2021-8-9到2021-8-28
 0 0,12,18,21 9-28 8 * node /scripts/jd_carnivalcity.js >> /scripts/logs/jd_carnivalcity.log 2>&1
 # 金机奖投票 活动时间：2021-8-9到2021-8-20
-33 4,7 8-20 8 * node /scripts/jd_golden_machine.js >> /scripts/logs/jd_golden_machine.log 2>&1
+13 0,7 8-20 8 * node /scripts/jd_golden_machine.js >> /scripts/logs/jd_golden_machine.log 2>&1
 # 女装盲盒 活动时间：2021-08-01到2021-08-31
 12 0 * 8 * node /scripts/jd_nzmh.js >> /scripts/logs/jd_nzmh.log 2>&1
 # 京东极速版红包(活动时间：2021-5-5至2021-5-31)
