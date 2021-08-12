172a78f57f6d19594d4d789bf6256d116cea828d
iceparis <80054093+iceparis@users.noreply.github.com>
2021-08-12 19:41:38 +0800

    更新

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -5,7 +5,7 @@
 
 ##############短期活动##############
 # 手机狂欢城 活动时间：2021-8-9到2021-8-28
-0 6,18 9-28 8 * node /scripts/jd_carnivalcity.js >> /scripts/logs/jd_carnivalcity.log 2>&1
+1 12,18 9-28 8 * node /scripts/jd_carnivalcity.js >> /scripts/logs/jd_carnivalcity.log 2>&1
 # 金机奖投票 活动时间：2021-8-9到2021-8-20
 13 0,7 8-20 8 * node /scripts/jd_golden_machine.js >> /scripts/logs/jd_golden_machine.log 2>&1
 # 女装盲盒 活动时间：2021-08-01到2021-08-31
@@ -14,10 +14,13 @@
 45 0,23 * * * node /scripts/jd_speed_redpocke.js >> /scripts/logs/jd_speed_redpocke.log 2>&1
 # 金榜创造营 活动时间：2021-05-21至2021-12-31
 0 1,22 * * * node /scripts/jd_gold_creator.js >> /scripts/logs/jd_gold_creator.log 2>&1
+# 5G超级盲盒(活动时间：2021-8-2到2021-10-29)
+1 0,6-23/3 * * * node /scripts/jd_mohe.js >> /scripts/logs/jd_mohe.log 2>&1
 # 天天加速
-8 0-23/3 * * * node /scripts/jd_speed.js >> /scripts/logs/jd_speed.log 2>&1
+38 12 * * * node /scripts/jd_speed.js >> /scripts/logs/jd_speed.log 2>&1
 # 省钱大赢家之翻翻乐(活动时间：2021-07-15至2021-8-15)
 20 * * * * node /scripts/jd_big_winner.js >> /scripts/logs/jd_big_winner.log 2>&1
+
 ##############长期活动##############
 # 签到
 1 0 * * * cd /scripts && node jd_bean_sign.js >> /scripts/logs/jd_bean_sign.log 2>&1
