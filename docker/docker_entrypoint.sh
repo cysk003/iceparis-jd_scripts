6fa595390f25bbad65b93826a70cff9f69bc33f3
iceparis <80054093+iceparis@users.noreply.github.com>
2021-08-29 18:18:01 +0800

    Update docker_entrypoint.sh

--- docker/docker_entrypoint.sh
+++ docker/docker_entrypoint.sh
@@ -15,7 +15,7 @@ else
   git remote set-url origin "$REPO_URL"
   git reset --hard
   echo "git pull拉取最新代码..."
-  git -C /scripts master
+  git pull origin $REPO_BRANCH
   echo "npm install 安装最新依赖"
   npm install --prefix /scripts
 fi
