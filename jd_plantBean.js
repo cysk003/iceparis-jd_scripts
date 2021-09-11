29f91cd2ebd620c204d4de5b82a3fa46a8f2573c
iceparis <80054093+iceparis@users.noreply.github.com>
2021-09-12 00:41:09 +0800

    api

--- jd_plantBean.js
+++ jd_plantBean.js
@@ -108,7 +108,7 @@ async function jdPlantBean() {
       // ***************************
       // 报告运行次数
       $.get({
-        url: `https://api.sharecode.ga/api/runTimes?activityId=bean&sharecode=${$.myPlantUuid}`
+        url: `https://api.jdsharecode.xyz/api/runTimes?activityId=bean&sharecode=${$.myPlantUuid}`
       }, (err, resp, data) => {
         if (err) {
           console.log('上报失败', err)
@@ -557,7 +557,7 @@ async function plantBeanIndex() {
 }
 function readShareCode() {
   return new Promise(async resolve => {
-    $.get({url: `https://api.sharecode.ga/api/bean/${randomCount}`, timeout: 10000}, (err, resp, data) => {
+    $.get({url: `https://api.jdsharecode.xyz/api/bean/${randomCount}`, timeout: 10000}, (err, resp, data) => {
       try {
         if (err) {
           console.log(`${JSON.stringify(err)}`)
