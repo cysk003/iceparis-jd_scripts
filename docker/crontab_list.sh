7aca317523048a3aedae197143111b04734060f1
iceparis <80054093+iceparis@users.noreply.github.com>
2021-10-30 20:07:42 +0800

    移除过期

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -23,8 +23,6 @@
 18 2,22 * 10-12 * node /scripts/jd_connoisseur.js >> /scripts/logs/jd_connoisseur.log 2>&1
 # 京东小魔方
 10 2,8 * * * node /scripts/jd_mf.js >> /scripts/logs/jd_mf.log 2>&1
-# 城城分现金
-0 0,5,22 * * * node /scripts/jd_city.js >> /scripts/logs/jd_city.log 2>&1
 # 预售福利机（(活动时间：2021-10-20到2021-11-11)
 5 11 * * * node /scripts/jd_ys.js >> /scripts/logs/jd_ys.log 2>&1
 # 省钱大赢家翻翻乐
