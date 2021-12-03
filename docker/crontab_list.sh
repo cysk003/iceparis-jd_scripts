cd3dcad5ffa1ebf80e349b3d15fe2188df428758
iceparis <80054093+iceparis@users.noreply.github.com>
2021-12-03 19:47:14 +0800

    Update crontab_list.sh

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -5,7 +5,7 @@
 
 ##############短期活动##############
 # 女装盲盒 活动时间：2021-11-01到2021-11-30
-15 1 * 11 * node /scripts/jd_nzmh.js >> /scripts/logs/jd_nzmh.log 2>&1
+15 8 * 12 * node /scripts/jd_nzmh.js >> /scripts/logs/jd_nzmh.log 2>&1
 # 京东极速版红包(活动时间：2021-5-5至2021-5-31)
 45 0,23 * * * node /scripts/jd_speed_redpocke.js >> /scripts/logs/jd_speed_redpocke.log 2>&1
 # 金榜创造营 活动时间：2021-05-21至2021-12-31
